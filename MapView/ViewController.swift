ea4fafbeaff584c8f9bf3a3ec2c31119167e0d97
Shahzod <bektemirovshahzodbek@gmail.com>
2021-03-02 12:30:46 +0500

    Initial Commit

---
+++ MapView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by shahzod on 3/2/21.
+//  Copyright © 2021 shahzod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
